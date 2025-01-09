465157b6fd34ad0137cab170bc7fe2dc5bea81c5
HFwas <1558934404@qq.com>
2025-01-09 20:12:36 +0800

    add: npm api

---
+++ devops-tool-api/src/main/java/com/hfwas/devops/tools/api/depency/NpmApi.java
@@ -0,0 +1,19 @@
+package com.hfwas.devops.tools.api.depency;
+
+import feign.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author houfei
+ * @package com.hfwas.devops.tools.api.depency
+ * @date 2025/1/9
+ */
+@FeignClient(name = "NpmApi", url = "${devops.depency.npm.url}")
+public interface NpmApi {
+
+    @GetMapping("/package/{name}/merge-streams?activeTab=dependencies")
+    Response dependents(@PathVariable("name") String name);
+
+}
