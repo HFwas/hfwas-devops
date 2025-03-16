40b07a643c628033ced9f02f42017776f3cb956f
HFwas <1558934404@qq.com>
2025-03-16 16:21:21 +0800

    fill BaseResult

--- devops-common/devops-common-core/src/main/java/com/hfwas/devops/common/core/base/BaseResult.java
+++ devops-common/devops-common-core/src/main/java/com/hfwas/devops/common/core/base/BaseResult.java
@@ -2,6 +2,7 @@ package com.hfwas.devops.common.core.base;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author houfei
@@ -10,8 +11,45 @@ import lombok.Data;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BaseResult<T> {
     private Integer code;
     private String msg;
     private T data;
+
+    public static <T> BaseResult<T> ok() {
+        return restResult(0, "", null);
+    }
+
+    public static <T> BaseResult<T> ok(T data) {
+        return restResult(0, null, data);
+    }
+
+    public static <T> BaseResult<T> ok(T data, String msg) {
+        return restResult(0, msg, data);
+    }
+
+    public static <T> BaseResult<T> failed() {
+        return restResult(1, null, null);
+    }
+
+    public static <T> BaseResult<T> failed(String msg) {
+        return restResult(1, msg, null);
+    }
+
+    public static <T> BaseResult<T> failed(T data) {
+        return restResult(1, null, data);
+    }
+
+    public static <T> BaseResult<T> failed(T data, String msg) {
+        return restResult(1, msg, data);
+    }
+
+    private static <T> BaseResult<T> restResult(int code, String msg, T data) {
+        BaseResult<T> apiResult = new BaseResult<>();
+        apiResult.setCode(code);
+        apiResult.setData(data);
+        apiResult.setMsg(msg);
+        return apiResult;
+    }
 }
