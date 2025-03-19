bfadfd2541c46d5fafd909531c6300c85cd918c7
HFwas <1558934404@qq.com>
2025-03-19 22:04:15 +0800

    add devops-base module

---
+++ devops-base/src/main/java/com/hfwas/devops/DevopsBaseApplication.java
@@ -0,0 +1,15 @@
+package com.hfwas.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DevopsBaseApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(DevopsBaseApplication.class, args);
+    }
+}
