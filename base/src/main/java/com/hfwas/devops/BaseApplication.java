6eaf9d7f3d9e89fd2f576d1d6be819c22f1e8892
HFwas <1558934404@qq.com>
2025-04-04 22:17:06 +0800

    update pom.xml

--- base/src/main/java/com/hfwas/devops/BaseApplication.java
+++ base/src/main/java/com/hfwas/devops/BaseApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class DevopsBaseApplication {
+public class BaseApplication {
     public static void main( String[] args ) {
-        SpringApplication.run(DevopsBaseApplication.class, args);
+        SpringApplication.run(BaseApplication.class, args);
     }
 }
