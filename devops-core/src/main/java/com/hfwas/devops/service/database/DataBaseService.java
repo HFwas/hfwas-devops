49492c338a0cb23103d0636d64bb83c8495245b4
HFwas <1558934404@qq.com>
2025-06-17 21:36:46 +0800

    add databases api

---
+++ devops-core/src/main/java/com/hfwas/devops/service/database/DataBaseService.java
@@ -0,0 +1,20 @@
+package com.hfwas.devops.service.database;
+
+import com.baomidou.mybatisplus.generator.config.po.TableField;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author houfei
+ * @package com.hfwas.devops.service.database
+ * @date 2025/6/17
+ */
+public interface DataBaseService {
+
+    List<String> schema();
+    Set<String> tables();
+    List<TableField> colums(String name);
+
+
+}
