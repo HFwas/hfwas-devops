42ea87d756cfeae2aa934ba8e012ee6147e67025
HFwas <1558934404@qq.com>
2025-06-18 22:40:15 +0800

    add repo api

---
+++ devops-core/src/main/java/com/hfwas/devops/service/repos/DevopsRepoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hfwas.devops.service.repos;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author hfwas
+ * @package com.hfwas.devops.service.repos
+ * @date 2025/6/18
+ */
+@Service
+public class DevopsRepoServiceImpl implements DevopsRepoService {
+}
